dff4b3afdb209e8f4040935d53776ff6663384b1
LannyRivero <lannyrivero288@gmail.com>
2025-01-25 01:06:27 +0100

    Add colorPicker and colorDisplay + styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import ColorPicker from "./components/ColorPicker"
-
+import React from "react";
+import "./App.css";
+import ColorDisplay from "./components/ColorDisplay/ColorDisplay";
+import ColorPicker from "./components/ColorPicker/ColorPicker";
+import { useColorPicker } from "./hooks/useColorPicker";
 
 function App() {
-  
+  const { selectedColor, handleColorChange } = useColorPicker();
 
   return (
-    <>
-      <ColorPicker />
-    </>
-  )
+    <div className="app">
+      
+      <ColorDisplay color={selectedColor} />
+      <ColorPicker color={selectedColor} handleColorChange={handleColorChange} />
+    </div>
+  );
 }
 
-export default App
+export default App;
+
